b76c4709c18ed812510e60ec1ff600f9cf0abe2e
malkiium <elihalldelafosse@gmail.com>
2026-09-08 21:06:41 +0200

    Add first and second successor functions with examples

--- 2627/01.hs
+++ 2627/01.hs
@@ -1,3 +1,11 @@
+firstsucc x y = succ x + succ y
+infi = succ 1 + succ 2
+pref1 = (+)(succ 1) (succ 2)
+
+secondsucc x y z = succ x + succ y + succ z
+infi2 = succ 1 + succ 2 + succ 3
+pref2 = (+)((+)(succ 1) (succ 2)) (succ 3)
+
 double x = x*2
 
 quadruple x = double (double x)
